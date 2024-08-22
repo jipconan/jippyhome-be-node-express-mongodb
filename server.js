1341c50111dbc09335e461bf7836bee584c86b3f
jipconan <jipconan@gmail.com>
2024-08-22 23:33:29 +0800

    feature/snipcart-setup-demo-v4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var cors = require('cors');
+var cors = require('cors');
 
 var securityMiddleware = require('./middlewares/security');
 
@@ -20,55 +20,14 @@ var finderRouter = require('./routes/finder');
 
 var app = express();
 
-// CORS configuration (initial)
-app.use(cors({
-  origin: '*', // Allow any origin
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type, Authorization, x-snipcart-publicapikey, x-snipcart-referer',
-  credentials: true,
-}));
-
-// Middleware for setting CORS headers and preflight requests
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://jippy.home.ngrok.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
-  res.setHeader("Access-Control-Allow-Headers", 
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  res.setHeader("Access-Control-Max-Age", 7200);
-  next();
-});
-
-// Preflight request handler
-app.options("*", (req, res) => {
-  console.log("Preflight request detected");
-  const allowMethods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"];
-  const allowHeaders = ["content-type", "authorization", "x-snipcart-publicapikey", "x-snipcart-referer"];
-
-  if (
-    req.headers.origin === "https://jippy.home.ngrok.app" &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("Preflight passed");
-    return res.status(204).send();
-  } else {
-    console.log("Preflight failed");
-    return res.status(403).send('CORS check failed');
-  }
-});
-
-// Middleware for logging, parsing, and security
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(securityMiddleware.checkJWT);
+app.use(securityMiddleware.checkJWT); 
 
-// Route handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
@@ -78,18 +37,20 @@ app.use('/cloudinary', cloudinaryRouter);
 app.use('/snipcart', snipcartRouter);
 app.use('/finder', finderRouter);
 
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// Error handler
+    next(createError(404));
+  });
+  
+// error handler
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
