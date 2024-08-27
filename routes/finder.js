f18621b366f1956ed76d99e35d023d2884c7f408
jipconan <jipconan@gmail.com>
2024-08-28 02:34:46 +0800

    feature/merchantpage-crud-v001

--- routes/finder.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productsCtrl = require('../controllers/products');
-
-// GET products grouped by subCategory for owner
-router.get('/', productsCtrl.getProductsBySubCategory);
-
-module.exports = router;
